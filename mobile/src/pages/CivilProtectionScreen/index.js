3619214d4a0432a6096c77fdcb96fdd9502c52dd
Edilson Lima <edilsonlm217@hotmail.com>
2020-12-20 15:45:07 -0400

    #1 Implementação básica da view

---
+++ mobile/src/pages/CivilProtectionScreen/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function CivilProtectionScreen() {
+  return (
+    <View>
+      <Text>CivilProtectionScreen</Text>
+    </View>
+  )
+}
---
+++ mobile/src/pages/ScheduleScreen/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ScheduleScreen() {
+  return (
+    <View>
+      <Text>ScheduleScreen</Text>
+    </View>
+  )
+}
